260712dd1977a9a7f0e3c3866a61b1232567eb1f
Gregor <gregor.mitscha-baude@gmx.at>
2021-06-22 14:10:15 +0200

    give bot some audience behaviour

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 import {createJam} from 'jam-core';
+import terminator from './terminator.json';
 
 const [state, api] = createJam({
   jamConfig: {
@@ -8,16 +9,44 @@ const [state, api] = createJam({
       turn: `turn:turn.jam.systems:3478`,
       turnCredentials: {username: 'test', credential: 'yieChoi0PeoKo8ni'},
     },
-    development: true,
+    development: false,
     sfu: true,
   },
-  debug: true,
+  debug: false,
 });
-const {createRoom, setProps, enterRoom} = api;
+const {setProps, enterRoom, onState, sendReaction, updateInfo} = api;
 
-console.log('myIdentity', JSON.stringify(state.myIdentity));
+console.log('myIdentity', JSON.stringify(state.myIdentity.info));
 
 let roomId = 'bot-test';
-createRoom(roomId);
 setProps({roomId});
 enterRoom(roomId);
+
+// with a probability 30%, set your own name & avatar within 30 sec
+if (Math.random() < 0.3) {
+  setTimeout(async () => {
+    updateInfo({
+      name: 'Unit ' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[(Math.random() * 26) | 0],
+      avatar: terminator,
+    });
+  }, 10000 + Math.random() * 20000);
+}
+
+// react with heart to someone speaking
+let nSpeaking = 0;
+onState('speaking', speaking => {
+  if (speaking.size > nSpeaking) {
+    sendReaction('❤️');
+  }
+  nSpeaking = speaking.size;
+});
+
+// random reaction storms
+setInterval(async () => {
+  if (Math.random() < 0.005) {
+    for (let i = 0; i < 20; i++) {
+      sendReaction('😅');
+      await new Promise(r => setTimeout(r, 50));
+    }
+  }
+}, 1000);
